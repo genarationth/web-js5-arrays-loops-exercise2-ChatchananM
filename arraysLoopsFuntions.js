2aea1ab7573e1fa478b2a443ab133be6a1dd956c
ChatchananM <165884301+ChatchananM@users.noreply.github.com>
2024-05-16 00:39:42 +0700

    First commit

---
+++ arraysLoopsFuntions.js
@@ -0,0 +1,18 @@
+//### Exercise #2
+
+// #### Part 1
+console.log("Exercise-2 Part 1");
+let waitingPeople = ["Sofia", "David", "Juan"];
+console.log(waitingPeople);
+
+// #### Part 2
+console.log("Exercise-2 Part 2");
+waitingPeople.push("Sara", "Augustin");
+waitingPeople.shift();
+console.log(waitingPeople);
+
+// #### Part 3
+console.log("Exercise-2 Part 3");
+waitingPeople.splice(1, 0, "Renata");
+waitingPeople.push("Elena");
+console.log(waitingPeople);
